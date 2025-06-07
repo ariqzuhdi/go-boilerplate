951ce69af561160a22f379da88ebe0b2e0b95ed0
cheeszy <ariqzhd@gmail.com>
2025-06-08 00:33:52 +0700

    fix fucvk

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,13 +2,20 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if _, err := os.Stat(".env"); err == nil {
+		// .env file exists, load it
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	} else {
+		// .env file does not exist, skipping loading
+		log.Println(".env file not found, skipping loading env file")
 	}
 }
